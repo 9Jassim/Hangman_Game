b7f310c4bedd56b7563dffb8a89b00ea9a3c276c
9Jassim <jassim9.a@gmail.com>
2024-01-18 14:01:47 +0300

    creat eventListener for letter buttons and check letter exist

--- gamescript.js
+++ gamescript.js
@@ -13,15 +13,34 @@ const getWord = () => {
   return 'hello'
 }
 
+const checkLetterExist = (letter) => {
+  const lettersDivs = document.querySelectorAll('.letters')
+  lettersDivs.forEach((div) => {
+    if (letter === div.getAttribute('id')) {
+      div.innerHTML = `${letter.toUpperCase()}`
+    }
+  })
+}
+
 const startRound = () => {
   let word = getWord()
 
   for (let i = 0; i < word.length; i++) {
     const letter = document.createElement('div')
-    letter.setAttribute('class', `${word[i]} letters`)
+    letter.setAttribute('class', `letters`)
+    letter.setAttribute('id', `${word[i]}`)
     letter.innerHTML = `_`
     wordDisplay.appendChild(letter)
   }
+
+  const lettersButtons = document.querySelectorAll('.letter')
+
+  lettersButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      checkLetterExist(button.getAttribute('id'))
+      button.setAttribute('disabled', true)
+    })
+  })
 }
 
 startRound()
