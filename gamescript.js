951585aadcfad23e87e8b9c58bd046bd30a54b9e
9Jassim <jassim9.a@gmail.com>
2024-01-18 13:28:52 +0300

    create function to display a word

--- gamescript.js
+++ gamescript.js
@@ -1,5 +1,27 @@
+const difficulty = localStorage.getItem('difficulty')
+const player = localStorage.getItem('player')
+
 const title = document.querySelector('h1')
-title.innerHTML += ` ${localStorage.getItem('difficulty')} level`
+title.innerHTML += ` ${difficulty} level`
 
 const playerTitle = document.querySelector('#player-title')
-playerTitle.innerHTML = `${localStorage.getItem('player')}`
+playerTitle.innerHTML = `${player}`
+
+const wordDisplay = document.querySelector('.word')
+
+const getWord = () => {
+  return 'hello'
+}
+
+const startRound = () => {
+  let word = getWord()
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = document.createElement('div')
+    letter.setAttribute('class', `${word[i]} letters`)
+    letter.innerHTML = `_`
+    wordDisplay.appendChild(letter)
+  }
+}
+
+startRound()
