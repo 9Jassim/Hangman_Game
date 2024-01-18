f56340718c92b09d2f2447ef6b0e947f3bf74d73
9Jassim <jassim9.a@gmail.com>
2024-01-18 14:44:49 +0300

    enable next round button if world revealed

--- gamescript.js
+++ gamescript.js
@@ -1,5 +1,6 @@
 const difficulty = localStorage.getItem('difficulty')
 const player = localStorage.getItem('player')
+let correct = 0
 
 const title = document.querySelector('h1')
 title.innerHTML += ` ${difficulty} level`
@@ -8,6 +9,10 @@ const playerTitle = document.querySelector('#player-title')
 playerTitle.innerHTML = `${player}`
 
 const wordDisplay = document.querySelector('.word')
+
+const lettersButtons = document.querySelectorAll('.letter')
+
+const nextButton = document.querySelector('#next')
 
 const getWord = () => {
   return 'hello'
@@ -18,11 +23,23 @@ const checkLetterExist = (letter) => {
   lettersDivs.forEach((div) => {
     if (letter === div.getAttribute('id')) {
       div.innerHTML = `${letter.toUpperCase()}`
+      correct++
     }
+  })
+  if (correct === lettersDivs.length) {
+    nextButton.disabled = false
+  }
+}
+
+const newRound = () => {
+  wordDisplay.innerHTML = ''
+  lettersButtons.forEach((button) => {
+    button.disabled = false
   })
 }
 
 const startRound = () => {
+  nextButton.disabled = true
   let word = getWord()
 
   for (let i = 0; i < word.length; i++) {
@@ -32,15 +49,18 @@ const startRound = () => {
     letter.innerHTML = `_`
     wordDisplay.appendChild(letter)
   }
-
-  const lettersButtons = document.querySelectorAll('.letter')
-
-  lettersButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      checkLetterExist(button.getAttribute('id'))
-      button.setAttribute('disabled', true)
-    })
-  })
 }
+
+lettersButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    checkLetterExist(button.getAttribute('id'))
+    button.setAttribute('disabled', true)
+  })
+})
+
+nextButton.addEventListener('click', () => {
+  newRound()
+  startRound()
+})
 
 startRound()
