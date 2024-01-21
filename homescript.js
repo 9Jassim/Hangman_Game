61ae6ec46aa3f84cb4ceb530cdec8a28e620a227
9Jassim <jassim9.a@gmail.com>
2024-01-21 10:42:44 +0300

    show selected difficulty

--- homescript.js
+++ homescript.js
@@ -1,12 +1,14 @@
 const difficulties = document.querySelectorAll('.difficulties')
 const userName = document.querySelector('#username')
 const start = document.querySelector('#start')
+const difficultyDisplay = document.querySelector('h3')
 let difficulty = ''
 
 difficulties.forEach((button) => {
   button.addEventListener('click', () => {
     difficulty = button.getAttribute('id')
     localStorage.setItem('difficulty', difficulty)
+    difficultyDisplay.innerHTML = `Difficulty: ${difficulty.toUpperCase()}`
   })
 })
 
